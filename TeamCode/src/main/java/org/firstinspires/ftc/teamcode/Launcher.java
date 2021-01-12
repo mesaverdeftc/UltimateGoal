5f4c5267eacac43dd89056f42d19741dda5867af
Bruce Meagher <bmeagher@glpsoftware.com>
2021-01-11 17:23:59 -0800

    Launcher support.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+public class Launcher {
+    double launcherPower = 0.0;
+
+    private DcMotor launcherMotor = null;
+    private Attachment launcherServo = new Attachment();
+
+    public void init(HardwareMap hardwareMap) {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        launcherMotor = hardwareMap.get(DcMotor.class, "launcher_motor_2");
+        launcherMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);  // TODO FIX WHEN ENCODER GETS INSTALLED
+        launcherMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // Coast when power is set to zero
+        launcherMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        launcherServo.init(hardwareMap, "launcher_servo_1", 0.0, 1.0);
+        launcherServo.up();
+    }
+
+    public void run(double power) {
+
+        launcherPower = Range.clip(power, -1.0, 1.0);
+        launcherMotor.setPower(launcherPower);
+    }
+
+    public void stop() {
+        launcherMotor.setPower(0);
+    }
+
+    public void launch(boolean toggleState) {
+        launcherServo.toggle(toggleState);
+    }
+}
