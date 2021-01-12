1a4a4dcb6c6315717fb7def769e3ae2658eaabf6
rotate0 <hudson.n.kim@gmail.com>
2021-01-11 16:38:06 -0800

    Debugging park auto on strafer robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkAuto.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="ParkAuto", group="Linear Opmode")
+//@Disabled
+public class ParkAuto extends LinearOpMode{
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    private DriveTrain driveTrain = new DriveTrain();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        driveTrain.init(hardwareMap);
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !driveTrain.imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("imu calib status", driveTrain.imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        //waiting for start
+        telemetry.addData("Mode", "waiting for start");
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        sleep(1000);
+        driveTrain.gyroDrive_constant(this, runtime, 0.2, 12, 0, 10, telemetry);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+    }
+}
